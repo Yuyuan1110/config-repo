5dcac142260051fd9a883d74b9ecebaf5af678ab
Yuyuan <z0975750027@gmail.com>
2024-06-12 17:48:12 +0800

    first commit

---
+++ src/test/java/com/configservertest/ConfigServerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.configservertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
